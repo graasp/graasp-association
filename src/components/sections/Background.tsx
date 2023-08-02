5db1e2311fc6c730d2a989210961256a65f7a572
spaenleh <spaenleh@gmail.com>
2023-08-02 16:11:09 +0200

    fix: use dynamic because of ui document bullshit

--- src/components/sections/Background.tsx
+++ src/components/sections/Background.tsx
@@ -1,29 +1,11 @@
 import React, { ReactElement } from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import BackgroundImage from 'gatsby-background-image';
+import { StaticImage } from 'gatsby-plugin-image';
 
 function Background({ children }: { children: ReactElement }) {
-  const data = useStaticQuery(graphql`
-    query {
-      background: file(
-        sourceInstanceName: { eq: "art" }
-        name: { eq: "background" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `);
   return (
-    <BackgroundImage
-      id="background"
-      fluid={data.background.childImageSharp.fluid}
-    >
+    <StaticImage src="../../images/art/background.png" alt="background image">
       {children}
-    </BackgroundImage>
+    </StaticImage>
   );
 }
 
